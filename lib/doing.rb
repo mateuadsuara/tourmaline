a2d3476feed1b44e48025c133372397f6bee2e47
Mateu Adsuara <mateuadsuara@gmail.com>
2015-09-13 06:00:51 +0100

    Adds fluent dsl to Doing

--- lib/doing.rb
+++ lib/doing.rb
@@ -1,11 +1,51 @@
 module Doing
   def doing(stop_value = nil, &producer)
-    Enumerator.new do |yielder|
+    enumerator = Enumerator.new do |yielder|
       loop do
         value = producer.call
         break if value.eql? stop_value
         yielder.yield value
       end
-    end.lazy
+    end
+
+    LazyEnum.new(enumerator)
+  end
+
+  private
+  class LazyEnum < Enumerator::Lazy
+    def initialize(enum)
+      super(enum) do |yielder, *values|
+        yielder.yield *values
+      end
+    end
+
+    def select(&predicate)
+      LazyEnum.new(super(&predicate))
+    end
+
+    def map(&transformation)
+      LazyEnum.new(super(&transformation))
+    end
+
+    def each(&side_effect)
+      return super(){} if side_effect.nil?
+      super(&side_effect)
+    end
+
+    def method_missing(method, *args)
+      return select{|e| e.send(method, *args)} if predicate?(method)
+      map do |e|
+        if e.respond_to?(method)
+          e.send(method, *args)
+        else
+          Kernel.send(method, e)
+        end
+      end
+    end
+
+    private
+    def predicate?(method)
+      method.to_s.end_with?("?")
+    end
   end
 end
--- spec/doing_spec.rb
+++ spec/doing_spec.rb
@@ -33,5 +33,64 @@ RSpec.describe Doing do
       enumerator = doing{values.shift.call}.map{|v| v+1}
       expect(enumerator.next).to eq(2)
     end
+
+    it 'can do something with each value' do
+      values = [1, 2, 3, 4]
+      acc = 0
+      returned_after_each = doing{values.shift}.each{|v| acc += v}
+      expect(acc).to eq(10)
+      expect(returned_after_each).to be(nil)
+    end
+
+    it 'consumes the lazyness when called each without a block' do
+      values = [1, 2, 3, 4]
+      acc = 0
+      enumerator = doing{values.shift}.map{|v| acc += v; v}
+      expect(acc).to eq(0)
+      returned_after_each = enumerator.each
+      expect(acc).to eq(10)
+      expect(returned_after_each).to be(nil)
+    end
+
+    it 'selects the elements that match a predicate finished in ?' do
+      values = ["ab", "ac", "bb", "bc"]
+      enumerator = doing{values.shift}.start_with?("ac")
+      expect(enumerator.next).to eq("ac")
+      expect{enumerator.next}.to raise_error(StopIteration)
+    end
+
+    it 'selects the elements that match several predicates' do
+      values = ["ab", "ac", "bb", "bc"]
+      enumerator = doing{values.shift}.start_with?("b").end_with?("b")
+      expect(enumerator.next).to eq("bb")
+      expect{enumerator.next}.to raise_error(StopIteration)
+    end
+
+    it 'maps the elements with a method that is not a predicate' do
+      values = [1, 2]
+      enumerator = doing{values.shift}.succ
+      expect(enumerator.next).to eq(2)
+      expect(enumerator.next).to eq(3)
+      expect{enumerator.next}.to raise_error(StopIteration)
+    end
+
+    it 'maps the elements with several methods that are not predicates' do
+      values = [1, 2]
+      enumerator = doing{values.shift}.succ.succ
+      expect(enumerator.next).to eq(3)
+      expect(enumerator.next).to eq(4)
+      expect{enumerator.next}.to raise_error(StopIteration)
+    end
+
+    it 'chaining an undefined method will asume is defined in Kernel' do
+      values = ["value"]
+      enumerator = doing{values.shift}.global_method
+      expect(enumerator.next).to eq("global method result for value")
+      expect{enumerator.next}.to raise_error(StopIteration)
+    end
+
+    def Kernel.global_method(argument)
+      "global method result for #{argument}"
+    end
   end
 end
